52c77066677c6665dc29f0dd57578648e9621a25
ElaineCRO <elainecro@gmail.com>
2019-12-21 21:23:45 -0300

    Removendo SignInManager, pois era só pra saber que existe.

--- WebApp.Identity/Controllers/HomeController.cs
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -16,14 +16,11 @@ namespace WebApp.Identity.Controllers
     {
         private readonly UserManager<MyUser> userManager;
         private readonly IUserClaimsPrincipalFactory<MyUser> _userClaimsPrincipalFactory;
-        private readonly SignInManager<MyUser> _signInManager;
 
-        public HomeController(UserManager<MyUser> userManager, IUserClaimsPrincipalFactory<MyUser> userClaimsPrincipalFactory,
-            SignInManager<MyUser> signInManager)
+        public HomeController(UserManager<MyUser> userManager, IUserClaimsPrincipalFactory<MyUser> userClaimsPrincipalFactory)
         {
             this.userManager = userManager;
             _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
-            _signInManager = signInManager;
         }
         public IActionResult Index()
         {
@@ -40,16 +37,13 @@ namespace WebApp.Identity.Controllers
         {
             if (ModelState.IsValid)
             {
-                //var user = await userManager.FindByNameAsync(model.UserName);
+                var user = await userManager.FindByNameAsync(model.UserName);
 
-                //if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
-                //{
-                //    var principal = await _userClaimsPrincipalFactory.CreateAsync(user);
+                if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var principal = await _userClaimsPrincipalFactory.CreateAsync(user);
 
-                //    await HttpContext.SignInAsync("Identity.Application", principal);
-
-                var signInResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
-                if (signInResult.Succeeded) { 
+                    await HttpContext.SignInAsync("Identity.Application", principal);
                     return RedirectToAction("About");
                 }
                 ModelState.AddModelError("", "Usuário ou Senha Inválida");
